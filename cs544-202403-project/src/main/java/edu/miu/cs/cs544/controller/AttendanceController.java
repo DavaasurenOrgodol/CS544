9861d3c2c41637784b95614ee3e14c65007165d2
Davaasuren Orgodol <616985@cs.mum.edu>
2024-03-18 11:56:31 -0500

    add an attendance controller

--- cs544-202403-project/src/main/java/edu/miu/cs/cs544/controller/AttendanceController.java
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/controller/AttendanceController.java
@@ -4,47 +4,32 @@ import edu.miu.cs.cs544.domain.*;
 import edu.miu.cs.cs544.repository.AttendanceRepository;
 import edu.miu.cs.cs544.repository.MemberRepository;
 import edu.miu.cs.cs544.repository.ScannerRepository;
+import edu.miu.cs.cs544.service.AttendanceService;
+import edu.miu.cs.cs544.service.contract.AttendancePayload;
+import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 @RestController
-@RequestMapping("/attendance")
+@RequestMapping("/attendances")
 public class AttendanceController {
-//    @Autowired
-//    ScannerRepository scannerRepository;
-//    @Autowired
-//    MemberRepository memberRepository;
-//    @Autowired
-//    AttendanceRepository attendanceRepository;
-//    @GetMapping("/register")
+    @Autowired
+    AttendanceService service;
+    @PostMapping("/register")
+    public ResponseEntity<?> attendance(@RequestBody AttendancePayload attendancePayload){
+        service.attendance(attendancePayload);
+        return new ResponseEntity<>(attendancePayload, HttpStatusCode.valueOf(200));
+    }
+//    @PostMapping("/register")
 //    public ResponseEntity<?> register(){
-//        Optional<Scanner> optionalScanner = scannerRepository.findById(1L);
-//        //findByBarCode
+////    receive an registerpayload request
 //        Optional<Member> optionalMember = memberRepository.findById(1L);
-//        if(optionalScanner.isPresent() && optionalMember.isPresent()){
-//            Member member = optionalMember.get();
-//            Scanner scanner = optionalScanner.get();
-//            Event event = scanner.getEvent();
-//            if(event.getParticipants().contains(member)){
-//                //
-//                AccountType accountType = scanner.getAccountType();
-//                Optional<Account> account = member.getAccounts()
-//                        .stream()
-//                        .filter((a)-> a.getType().equals(accountType))
-//                        .findFirst();
-//                //withdraw point from this account
-//                //account.withdraw();
-//                Attendance attendance = new Attendance();
-//                attendance.setMember(member);
-//                attendance.setScanner(scanner);
-//                attendanceRepository.save(attendance);
-//            }
-//        }
-//
+////        Optional<Event> eventOptional = eventRepository.findById(1L);
+////        Event event = eventOptional.get();
+////        event.addParticipant(optionalMember.get());
 //        return null;
 //    }
 }
