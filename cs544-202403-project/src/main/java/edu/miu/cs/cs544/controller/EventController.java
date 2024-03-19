8046679178e1f52c8a428c14137a59573f88fb07
michaltesfu <ghebrekidanmichal@gmail.com>
2024-03-19 18:54:26 -0500

    commit-1

--- cs544-202403-project/src/main/java/edu/miu/cs/cs544/controller/EventController.java
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/controller/EventController.java
@@ -1,23 +1,33 @@
 package edu.miu.cs.cs544.controller;
 
 import edu.miu.common.controller.BaseReadWriteController;
+import edu.miu.cs.cs544.domain.Attendance;
 import edu.miu.cs.cs544.domain.Event;
+import edu.miu.cs.cs544.domain.Member;
+import edu.miu.cs.cs544.service.EventService;
 import edu.miu.cs.cs544.service.contract.EventPayload;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/events")
 public class EventController extends BaseReadWriteController<EventPayload, Event, Long>{
 
-//    private final AttendanceServiceImpl attendanceService;
-//    public EventController(AttendanceServiceImpl attendanceService) {
-//        this.attendanceService = attendanceService;
-//    }
-//
-//    @GetMapping("/{eventId}/attendance")
-//    public int getAttendance(@PathVariable long eventId) {
-//        return attendanceService.calculateAttendance(eventId);
-//    }
+    private final EventService eventService;
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+@GetMapping("/{eventId}/attendance")
+public ResponseEntity<Map<Member, List<Attendance>>> calculateAttendance(@PathVariable Long eventId) {
+    Map<Member, List<Attendance>> attendanceMap = eventService.calculateAttendance(eventId);
+    return ResponseEntity.ok(attendanceMap);
 }
+}
+
 
