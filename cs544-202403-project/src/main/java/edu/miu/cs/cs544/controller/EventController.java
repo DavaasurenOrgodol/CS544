1088abcd8fd4e57c54fc51a2971e5071c5a1a754
Davaasuren Orgodol <616985@cs.mum.edu>
2024-03-19 19:53:18 -0500

    fixed merge conflict

--- cs544-202403-project/src/main/java/edu/miu/cs/cs544/controller/EventController.java
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/controller/EventController.java
@@ -4,9 +4,12 @@ import edu.miu.common.controller.BaseReadWriteController;
 import edu.miu.cs.cs544.domain.Attendance;
 import edu.miu.cs.cs544.domain.Event;
 import edu.miu.cs.cs544.domain.Member;
+import edu.miu.cs.cs544.domain.Session;
 import edu.miu.cs.cs544.service.EventService;
+import edu.miu.cs.cs544.service.SessionService;
 import edu.miu.cs.cs544.service.contract.EventPayload;
 
+import edu.miu.cs.cs544.service.contract.SessionPayload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -45,26 +48,27 @@ public class EventController extends BaseReadWriteController<EventPayload, Event
     @PutMapping("{eventId}/sessions/{sessionId}")
     public ResponseEntity<?> UpdateSession(@PathVariable long eventId,
                                            @PathVariable long sessionId,
-                                           @RequestBody SessionPayload sessionPayload)
-    {
-        return sessionService.updateSessionByEventIdAndSessionId(eventId,sessionId,sessionPayload);
+                                           @RequestBody SessionPayload sessionPayload) {
+        return sessionService.updateSessionByEventIdAndSessionId(eventId, sessionId, sessionPayload);
     }
 
     @DeleteMapping("{eventId}/sessions/{sessionId}")
     public ResponseEntity<?> deleteSessionById(@PathVariable long eventId,
-                                            @PathVariable long sessionId) {
-        return sessionService.deleteSessionByEventIdAndSessionId(eventId,sessionId);
+                                               @PathVariable long sessionId) {
+        return sessionService.deleteSessionByEventIdAndSessionId(eventId, sessionId);
     }
+
     private final EventService eventService;
+
     public EventController(EventService eventService) {
         this.eventService = eventService;
     }
 
-@GetMapping("/{eventId}/attendance")
-public ResponseEntity<Map<Member, List<Attendance>>> calculateAttendance(@PathVariable Long eventId) {
-    Map<Member, List<Attendance>> attendanceMap = eventService.calculateAttendance(eventId);
-    return ResponseEntity.ok(attendanceMap);
-}
+    @GetMapping("/{eventId}/attendance")
+    public ResponseEntity<Map<Member, List<Attendance>>> calculateAttendance(@PathVariable Long eventId) {
+        Map<Member, List<Attendance>> attendanceMap = eventService.calculateAttendance(eventId);
+        return ResponseEntity.ok(attendanceMap);
+    }
 }
 
 
